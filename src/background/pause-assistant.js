1b9d3b87f9cb6e9644270cb6078f339f3913d657
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-07-12 15:43:05 -0500

    simplify pause assistant flow

--- src/background/pause-assistant.js
+++ src/background/pause-assistant.js
@@ -16,58 +16,65 @@ import Config, {
   ACTION_PAUSE_ASSISTANT,
   FLAG_PAUSE_ASSISTANT,
 } from '/store/config.js';
-import Options, { getPausedDetails } from '/store/options.js';
+import Options from '/store/options.js';
 
 import { openNotification } from './notifications.js';
 
-// Detect "pause" action and trigger pause assistant or feedback
-chrome.webNavigation.onCompleted.addListener(async (details) => {
-  const config = await store.resolve(Config);
+store.observe(Config, async (_, config) => {
   if (!config.hasFlag(FLAG_PAUSE_ASSISTANT)) return;
 
-  if (details.frameId === 0) {
-    const hostname = parse(details.url).hostname;
-    if (!hostname) return;
-
-    const options = await store.resolve(Options);
-    const paused = getPausedDetails(options, hostname);
-    const hasAction = config.hasAction(hostname, ACTION_PAUSE_ASSISTANT);
-
-    if (!paused) {
-      if (hasAction) {
-        // The site is not paused, but the domain with an action is in the config
-        openNotification(details.tabId, 'pause-assistant', { hostname });
+  const paused = Object.entries(config.domains).reduce(
+    (acc, [domain, { actions, dismiss }]) => {
+      if (
+        actions.includes(ACTION_PAUSE_ASSISTANT) &&
+        !dismiss[ACTION_PAUSE_ASSISTANT]
+      ) {
+        acc = acc || {};
+        acc[domain] = { revokeAt: 0, assist: true };
       }
-    } else if (!hasAction && paused.assist) {
-      // the site is paused with the assistant, but the domain is not in the config
-      openNotification(details.tabId, 'pause-resume', { hostname });
-    }
+      return acc;
+    },
+    null,
+  );
+
+  if (paused) {
+    store.set(Options, { paused });
   }
 });
 
-const RELOAD_DELAY = 1 * 1000; // 1 second
+chrome.webNavigation.onCompleted.addListener(async (details) => {
+  if (details.frameId === 0) {
+    const config = await store.resolve(Config);
+    if (!config.hasFlag(FLAG_PAUSE_ASSISTANT)) return;
 
-// Listen to pause assistant messages
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  switch (msg.action) {
-    case 'config:pause:reload': {
-      setTimeout(() => chrome.tabs.reload(sender.tab.id), RELOAD_DELAY);
+    const hostname = parse(details.url).hostname;
+    if (!hostname) return;
 
-      const cb = (details) => {
-        if (details.frameId === 0 && details.tabId === sender.tab.id) {
-          setTimeout(
-            () => openNotification(sender.tab.id, 'pause-feedback', msg.params),
-            msg.delay,
-          );
-          chrome.webNavigation.onDOMContentLoaded.removeListener(cb);
-        }
-      };
+    const hasAction = config.hasAction(hostname, ACTION_PAUSE_ASSISTANT);
 
-      chrome.webNavigation.onDOMContentLoaded.addListener(cb);
-      break;
-    }
-    case 'config:pause:feedback': {
-      // TODO: handle feedback
+    if (hasAction && !config.isDismissed(hostname, ACTION_PAUSE_ASSISTANT)) {
+      // The page is loaded, show the pause assistant notification
+      openNotification({
+        id: 'pause-assistant',
+        tabId: details.tabId,
+        position: 'center',
+        params: { hostname },
+      });
+    } else {
+      const options = await store.resolve(Options);
+      const domain = Object.keys(options.paused).find((d) =>
+        hostname.endsWith(d),
+      );
+
+      if (!hasAction && options.paused[domain]?.assist) {
+        // The page is loaded, show the pause resume notification
+        openNotification({
+          id: 'pause-resume',
+          tabId: details.tabId,
+          params: { domain },
+          position: 'center',
+        });
+      }
     }
   }
 });
