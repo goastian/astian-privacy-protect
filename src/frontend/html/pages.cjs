730af1fc48ca162dc5cb2040865978d9a08d5116
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-07-04 02:15:12 -0500

    refactor include modiles export

--- src/frontend/html/pages.cjs
+++ src/frontend/html/pages.cjs
@@ -1,15 +1,1 @@
-/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
-/* eslint-disable no-undef */
-// This file is used by webpack to get a list of html pages to copy to the dist
-// directory. That is why this isn't typescript and uses commonjs exports
-
-const fs = require('fs')
-const path = require('path')
-
-const pages = fs
-  .readdirSync(__dirname, { withFileTypes: true })
-  .filter((file) => file.isFile())
-  .map((file) => file.name)
-  .filter((file) => path.extname(file) === '.html')
-
-module.exports = pages
+module.exports = ['blocked.html', 'popup.html', 'settings.html', 'stats.html']
